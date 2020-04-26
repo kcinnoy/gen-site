30477b3debb13044f596530816474081c8a2d454
kcinnoy <yonnick00@gmail.com>
2020-04-27 00:44:26 +0100

    updated error json object

--- gen_api/validators/index.js
+++ gen_api/validators/index.js
@@ -2,8 +2,20 @@ const { validationResult } = require('express-validator');
 
 exports.runValidation = (req, res, next) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ error: errors.array()[0].msg });
+    // if (!errors.isEmpty()) {
+    //   return res.status(422).json({ error: errors.array()[0].msg });
+    // }
+    // next();
+
+    if (errors.isEmpty()) {
+      return next()
     }
-    next();
+    const extractedErrors = []
+    errors.array().map(err => extractedErrors.push(err.msg))
+
+
+    return res.status(422).json({
+      errors: extractedErrors,
+    })
+
 };

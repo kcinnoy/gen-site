c2107af8859b07d261f54dd277c76e863c8f48e1
kcinnoy <yonnick00@gmail.com>
2020-04-29 23:09:44 +0100

    create next.js app

--- gen_react/.next/static/runtime/webpack.js
+++ gen_react/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f882fa751edb05bc843b";
+/******/ 	var hotCurrentHash = "dee8fb8a3f74ce5e32d2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

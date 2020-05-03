421817378ee7ab348a6e297da7b8b644fc3c9028
kcinnoy <yonnick00@gmail.com>
2020-05-03 15:25:45 +0100

    added basic user form

--- gen_react/.next/static/runtime/webpack.js
+++ gen_react/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c72f1359ffca5b479b73";
+/******/ 	var hotCurrentHash = "c9aff8dbeff00767cc56";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
